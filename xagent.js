cfe852ff6c0f9a503d67cacd1d063a96816b6e2c
Marc Rumo <marc.rumo@gmail.com>
2019-01-09 17:18:46 +0100

    adds a NOOP setter

--- xagent.js
+++ xagent.js
@@ -1,17 +1,24 @@
 (function () {
     var defProp = function (prop, value) {
-        Object.defineProperty(window.navigator, prop, {
-            get: function () {
-                return value;
-            }
-        });
+		try
+		{
+			Object.defineProperty(window.navigator, prop, {
+				get: function () {
+					return value;
+				},
+				set: function() { console.log('shK3Bq4d/X-Agent Someone else tried to sed me');}
+			});
+		}
+		catch (f)
+		{	console.log('shK3Bq4d/X-Agent f', f);
+		}
     };
 	var bHO;
 	try
 	{	bHO = get_bHO(host_from_url(location.host));
 	}
 	catch (e)
-	{
+	{	console.log('shK3Bq4d/X-Agent e', e);
 	}
 
 	if (bHO)
